df19caa829a7f3c2fec2dc42870d85f9453bc4f6
Himanshu Ramavat <himanshuramavat07@gmail.com>
2025-03-16 12:24:35 +0530

    Update task and solution scripts with interactive input features

--- Day 1/task/solution.py
+++ Day 1/task/solution.py
@@ -1,2 +1,5 @@
-# Write your code below this line 👇
-print("Hello world!")
+# Band Name Generator
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be " + city + " " + pet)
--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,3 +1,10 @@
 # Write your code below this line 👇
-print("Hello World!")
+# print("Hello World!")
+# print("Hello world \nHello World")
+# print("Hello" + "Himanshu")
 
+# print("Hello " + input("What is your name?") + "!")
+
+name = input("What is your name?")
+print("Hello " + name)
+print(len(name))
